b4fe5130acdc4dbb3d82506c63c4626786eb4d64
Jordan Winslow <admin@jordanwinslow.me>
2019-08-10 00:33:03 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -15,12 +15,11 @@ function init() {
   let currentKeyCount = 0
   document.body.addEventListener("keydown", function(pressed){
     if(pressed.key === codes[currentKeyCount]) {
+        currentKeyCount++
       if(currentKeyCount === codes.length) {
         alert("OMGAWD YOU PRESSED SOME BUTTONS");
         currentKeyCount = 0
-      } else {
-        currentKeyCount++
-      }
+      } 
     }
   })
 }
